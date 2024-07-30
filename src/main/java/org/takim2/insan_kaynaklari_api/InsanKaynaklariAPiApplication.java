ef8ed9192ea6a4a1ab8d028b7c7ef66b4f6d68e7
ertugrulsaliher <ertugrulsaliher@gmail.com>
2024-07-30 13:32:50 +0300

    Initial commit

---
+++ src/main/java/org/takim2/insan_kaynaklari_api/InsanKaynaklariAPiApplication.java
@@ -0,0 +1,13 @@
+package org.takim2.insan_kaynaklari_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsanKaynaklariAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsanKaynaklariAPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/takim2/insan_kaynaklari_api/InsanKaynaklariAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.takim2.insan_kaynaklari_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsanKaynaklariAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
